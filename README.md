bf908fdc222ca414fa7579a5760dcf2d75701c3b
Michael Auteri <mauteri@gmail.com>
2013-06-04 23:47:54 -0400

    First commit. Not fully tested and very much in an alpha state.

--- README.md
+++ README.md
@@ -1,4 +1,52 @@
-confirmify
-==========
+# Confirmify #
 
-Confirmation and Notification Plugin for jQuery
+Confirmation and Notification Plugin for jQuery. It's a nicer alternative to using alert() and confirm().
+
+# Basic Usage #
+
+## For Notifications ##
+
+	$.confirmify({
+		message: 'Here is a notification!'	
+	});
+
+## For Confirmation ##
+
+	$.confirmify({
+		message: 'Are you sure?',
+		callback: someFunction
+	});
+
+or assign an anonymous function
+
+	$.confirmify({
+		message: 'Are you sure?',
+		callback: function() { 
+			alert('I am sure!'); 
+		}
+	});
+
+# Configurable Options #
+
+	* type **default 'info' - options include 'success', 'warning', 'error', 'danger', 'info')**
+	* title **default empty**
+	* message **default empty**
+	* duration **default 5000 (milliseconds) - set to false for no duration**
+	* fadeSpeed **default 500**
+	* close **default true**
+	* width **default '400px' - options include any width measurement**
+	* position **default is fixed**
+	* z_index **default is 9999**
+	* callback **default is false - option must be a function for a confirmation**
+
+## Coming Soon ##
+	* keyboard **these will be keyboard bindings and will default to true**
+		* enter
+		* esc
+	* cancelFocus **this will set primary button to Cancel rather than OK**
+
+# Version History #
+
+## 0.1 ##
+   
+	 * First release. Not fully tested and very much in an alpha state. Currently relies on [Twitter Bootstrap](http://twitter.github.io/bootstrap/) for styling. Started separating out needed styles into a lighter weight stylesheet.
---
+++ js/jquery.confirmify.js
@@ -0,0 +1,139 @@
+/**
+ * jquery.confirmify.js v0.1
+ *
+ * @author mauteri <mauteri@gmail.com>
+ * 
+ */
+(function( $ ) {
+
+	$.confirmify = function( options ) {
+		var $self = $.confirmify;
+
+		$self.queue = typeof $self.queue === 'undefined' ? [] : $self.queue;
+		
+		var defaults = {
+			type: 'info',
+			title: '',
+			message: '',
+			keyboard: {
+				enter: true,
+				esc: true
+			},
+			duration: 5000,
+			fadeSpeed: 500,
+			close: true,
+			width: '400px',
+			position: 'fixed',
+			cancelFocus: false,
+			z_index: 9999,
+			callback: false
+		};
+
+		var settings = $.extend( {}, defaults, options );
+		var html = $('<div />');
+		html.addClass('confirmify');
+		html.animate({ opacity: 0 }, 0);
+		html.width(settings.width);
+		html.addClass('alert');
+		
+		// Make sure if callback is set that it's a function
+		if( settings.callback && typeof settings.callback !== 'function' ) {
+			settings.callback = false;
+		}
+
+		// Check if close X should appear. If there's a callback, an X will not appear
+		if( ( settings.close || !settings.duration ) && !settings.callback ) {
+			html.prepend('<button type="button" class="close confirmify-cancel" data-dismiss="alert">&times;</button>');
+		} else {
+			html.css({ 'padding': '10px' });
+		}
+
+		switch(settings.type) {
+			case 'success':
+				html.addClass('alert-success');
+				break;
+			case 'warning':
+				html.addClass('alert-warning');
+				break;
+			case 'error':
+				html.addClass('alert-error');
+				break;
+			case 'danger':
+				html.addClass('alert-danger');
+				break;
+			case 'info':
+			default:
+				html.addClass('alert-info');
+		}
+		if( settings.title !== '' ) {
+			html.append('<h4>'+settings.title+'</h4>');
+		}
+		html.append('<p>'+settings.message+'</p>');
+	
+		// If callback is NOT a function, it will be false and this will not run
+		if( settings.callback ) {
+			html.append('<div class="confirmify-btns pull-right" />');
+			html.find('.confirmify-btns').append('<button class="confirmify-cancel btn btn-small">Cancel</button>&nbsp;');
+			html.find('.confirmify-btns').append('<button class="confirmify-ok btn btn-primary btn-small">OK</button>');
+		}
+
+		// Add html to settings object and then add object to queue array
+		settings.html = html;
+		$self.queue.push(settings);
+
+		// If $self.runQueue is undefined, that means there is only one item in the queue
+		if( typeof $self.runQueue === 'undefined' ) {
+			$self.runQueue = function(current) {
+				$(document.body).prepend(current.html);
+				current.html
+					.css({ 
+						'top': '-'+parseInt(current.html.height(), 10)+'px', 
+						'margin-left': '-'+parseInt(current.html.outerWidth(), 10)/2+'px',
+						'left': '50%',
+						'position': current.position,
+						'z-index': current.z_index
+					})
+					.animate({ 'opacity': 1, 'top': '10px' }, current.fadeSpeed);
+			
+				// Remove item that just ran from queue
+				$self.queue.splice(0, 1);
+				
+				// Check if notification (no callback) or confirmation (callback)
+				if( !current.callback && current.duration ) {
+					var delay = setTimeout( function() { queueFinish(current) }, current.duration );
+				} else {
+					current.html.on('click', '.confirmify-ok', function() {
+						current.callback();
+						queueFinish(current);
+					});
+				}
+				
+				current.html.on('click', '.confirmify-cancel', function(e) {
+					e.stopPropagation();
+					if( typeof delay !== 'undefined' ) {
+						clearTimeout(delay);
+					}
+					queueFinish(current);
+				});
+
+			}
+			$self.runQueue($self.queue[0]);
+		}
+		
+		// Internal Methods
+		function queueFinish(current) {
+			current.html.fadeOut(current.fadeSpeed, 'linear', function() { 
+				current.html.remove();
+				
+				// Check queue if $self.runQueue should run again, if not, set it as undefined so another can run at any time
+				if( typeof $self.queue[0] !== 'undefined' ) {
+					$self.runQueue($self.queue[0]);
+				} else {
+					$self.runQueue = undefined;
+				}
+			});
+		}
+		
+	};
+
+})( jQuery );
